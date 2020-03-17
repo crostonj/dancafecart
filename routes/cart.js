29d6d61f0969dcddad8b97ba28f7f40168cc3f1d
Jeff Croston <crostonj@gmail.com>
2020-03-16 22:18:48 -0600

    updated get Carts response

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var items = getCartItems();
+  res.send(items);
 });
 
 router.post('/', function(req, res, next) {
